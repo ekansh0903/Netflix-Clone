ab48313f0ebbf4a95da1bcfa2e973e881882d0e2
ekansh0903 <ekansh0903@gmail.com>
2024-09-01 20:59:12 +0530

    Minor changes

--- Frontend/src/pages/WatchPage.jsx
+++ Frontend/src/pages/WatchPage.jsx
@@ -23,7 +23,7 @@ const WatchPage = () => {
 	useEffect(() => {
 		const getTrailers = async () => {
 			try {
-				const res = await axios.get(`/api/v1/${contentType}/${id}/trailers`);
+				const res = await axios.get(`https://netflix-clone-vdcs.onrender.com/api/v1/${contentType}/${id}/trailers`);
 				setTrailers(res.data.trailers);
 			} catch (error) {
 				if (error.message.includes("404")) {
@@ -38,7 +38,7 @@ const WatchPage = () => {
 	useEffect(() => {
 		const getSimilarContent = async () => {
 			try {
-				const res = await axios.get(`/api/v1/${contentType}/${id}/similar`);
+				const res = await axios.get(`https://netflix-clone-vdcs.onrender.com/api/v1/${contentType}/${id}/similar`);
 				setSimilarContent(res.data.similar);
 			} catch (error) {
 				if (error.message.includes("404")) {
@@ -53,7 +53,7 @@ const WatchPage = () => {
 	useEffect(() => {
 		const getContentDetails = async () => {
 			try {
-				const res = await axios.get(`/api/v1/${contentType}/${id}/details`);
+				const res = await axios.get(`https://netflix-clone-vdcs.onrender.com/api/v1/${contentType}/${id}/details`);
 				setContent(res.data.content);
 			} catch (error) {
 				if (error.message.includes("404")) {
